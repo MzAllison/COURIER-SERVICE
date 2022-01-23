ea9dbbef150a4d66052ea8644e7dea77a1f0a409
MzAllison <isituaho@gmail.com>
2022-01-23 11:28:58 +0100

    MenuData

---
+++ src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+    {title: 'Home', link: '/home'},
+    {title: 'About Us', link: '/about'},
+    {title: 'Services', link: '/services'},
+    {title: 'Our Blog', link: '/blog'}
+];
