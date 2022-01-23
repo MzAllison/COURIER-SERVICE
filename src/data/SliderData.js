8b3dd053565a08161bc5aed2dcadd1110fea98bb
MzAllison <isituaho@gmail.com>
2022-01-23 11:29:36 +0100

    Slider

---
+++ src/data/SliderData.js
@@ -0,0 +1,68 @@
+import ImageOne from '../images/courier-1.jpeg'
+import ImageTwo from '../images/courier-2.jpeg'
+import ImageThree from '../images/courier-3.jpeg'
+import ImageFour from '../images/courier-4.jpeg'
+
+
+
+export const SliderData = [
+    {
+        title: 'Overland Network in lagos, Nigeria',
+        price: 'N10,000',
+        path: '/home',
+        label: 'View Trucks',
+        image: ImageOne,
+        alt: 'Home'
+    },
+    {
+        title: 'Overland Network in lagos, Nigeria',
+        price: 'N10,000',
+        path: '/home',
+        label: 'View Trucks',
+        image: ImageTwo,
+        alt: 'Home'
+    },
+    {
+        title: 'Overland Network in lagos, Nigeria',
+        price: 'N10,000',
+        path: '/home',
+        label: 'View Trucks',
+        image: ImageThree,
+        alt: 'Home'
+    },
+    {
+        title: 'Overland Network in lagos, Nigeria',
+        price: 'N10,000',
+        path: '/home',
+        label: 'View Trucks',
+        image: ImageFour,
+        alt: 'Home'
+    }
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'Overland Network in lagos, Nigeria',
+//         price: 'N10,000',
+//         path: '/home',
+//         label: 'View Trucks',
+//         image: ImageFive,
+//         alt: 'Home'
+//     },
+//     {
+//         title: 'Overland Network in lagos, Nigeria',
+//         price: 'N10,000',
+//         path: '/home',
+//         label: 'View Trucks',
+//         image: ImageSix,
+//         alt: 'Home'
+//     },
+//     {
+//         title: 'Overland Network in lagos, Nigeria',
+//         price: 'N10,000',
+//         path: '/home',
+//         label: 'View Trucks',
+//         image: ImageSeven,
+//         alt: 'Home'
+//     }
+// ];
